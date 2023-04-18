fe329abe21c154e6a176d4cfc6bbfe1e29dc22de
Lumaa <nathan@ama49.fr>
2023-04-18 15:10:11 +0200

    useless packet

--- src/main/java/com/lumaa/act/packet/NPCPackets.java
+++ src/main/java/com/lumaa/act/packet/NPCPackets.java
@@ -5,5 +5,4 @@ import net.minecraft.util.Identifier;
 
 public class NPCPackets {
     public static final Identifier SET_GAMEPROFILE = new Identifier(ActMod.MODID, "set_gameprofile");
-    public static final Identifier APPEAR = new Identifier(ActMod.MODID, "appear");
 }
