f0a1960c4de674a8a19c3945b1c97f016706f5d9
Lumaa <nathan@ama49.fr>
2023-04-22 21:33:41 +0200

    tried to fix

--- src/main/java/com/lumaa/act/item/stick/InventoryStick.java
+++ src/main/java/com/lumaa/act/item/stick/InventoryStick.java
@@ -1,19 +1,18 @@
 package com.lumaa.act.item.stick;
 
-import com.lumaa.act.ai.ActorAction;
 import com.lumaa.act.entity.ActorEntity;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 
+@Environment(EnvType.CLIENT)
 public class InventoryStick extends Item {
     public InventoryStick(Settings settings) {
         super(settings.maxCount(1));
@@ -23,8 +22,11 @@ public class InventoryStick extends Item {
     public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
         if (entity instanceof ActorEntity) {
             ActorEntity actor = (ActorEntity) entity;
+            InventoryScreen inv = new InventoryScreen(user);
+
             MinecraftClient mc = MinecraftClient.getInstance();
-            mc.setScreen(new InventoryScreen(actor));
+            mc.setScreen(inv);
+
             return ActionResult.SUCCESS;
         }
         return ActionResult.FAIL;
